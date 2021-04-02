9c083ffd5a38b7ce53b6ec65cd9e28895e45f1df
Finno <funsinaime@gmail.com>
2021-04-01 21:03:38 -0400

    add the script to run the snakemake pipeline with slurm system

---
+++ submit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+module load snakemake samtools || exit 1
+
+mkdir -p logs/slurm
+sbcmd="sbatch --cpus-per-task={threads} --mem={resources.mem}"
+sbcmd+=" --time={cluster.time} --partition={cluster.partition}"
+sbcmd+=" --out={snakedir}/logs/slurm/{rule}.%j.out {resources.extra}"
+
+snakemake -pr --keep-going --local-cores 4 \
+    --jobs 999 --cluster-config cluster.yaml --cluster "$sbcmd" \
+    --latency-wait 120 all --configfile config.yaml --snakefile Snakefile
